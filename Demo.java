bdacc67c457232e660e9ea6c5ed749eb4738f95d
Shaijo George <97226113+ShaijoGeorge@users.noreply.github.com>
2023-01-23 22:57:29 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args)
+    {
+        int a=9;
+        int b=7;
+        System.out.println(b);
+        b=a++ + a++;
+        System.out.println(a);
+    }
+}
+//instagram demo
